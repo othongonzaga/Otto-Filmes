ec4b26cf3987d2b37164b7099612538586135713
othongonzaga <othoncesar1@hotmail.com>
2021-08-26 17:16:21 -0300

    mudando

--- otto-filmes/src/services/api.js
+++ otto-filmes/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 // Base Url: https:sujeitoprogramador.com/r-api/?api=filmes/
 const api = axios.create({
-    baseURL: 'https:sujeitoprogramador.com'
+    baseURL: 'https://sujeitoprogramador.com'
 })
 
 export default api
